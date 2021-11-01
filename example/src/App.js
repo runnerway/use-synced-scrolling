f508158f3a2999aa6e754c80404fc4b5cb7c64f6
Andrea Torlaschi <runnerway@gmail.com>
2021-11-01 10:56:10 +0100

    Improve example and readme

--- example/src/App.js
+++ example/src/App.js
@@ -2,63 +2,57 @@ import React, { useState } from "react";
 import { useSyncedScrolling } from "use-synced-scrolling";
 import "./App.css";
 
+const text =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae eleifend elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi ut orci a ex molestie scelerisque. Proin quis urna tincidunt, pharetra justo in, posuere dolor. Mauris placerat orci in nulla tempus, efficitur elementum mi vulputate. Ut accumsan sit amet sem vel cursus. Etiam eu magna diam. Donec vulputate tellus dui, vitae accumsan est efficitur id. Phasellus dapibus, magna non vehicula suscipit, nisl nulla lobortis neque, sed dapibus erat dolor et quam. Aenean laoreet, ante ac cursus fermentum, felis erat convallis erat, vel sagittis leo libero nec magna. Phasellus viverra feugiat ullamcorper. Duis volutpat suscipit nisi. Suspendisse interdum leo eu facilisis facilisis. Nullam vestibulum eu odio et finibus.";
+
 export default function App() {
-  const syncedScrolling = useSyncedScrolling();
-  const [checked, setChecked] = useState(false);
   return (
-    <div>
+    <>
+      <Example />
+      <DynamicComponent />
+    </>
+  );
+}
+
+function Example() {
+  const syncedScrolling = useSyncedScrolling();
+  return (
+    <main>
+      <div {...syncedScrolling}>
+        <div>{text}</div>
+      </div>
+      <div {...syncedScrolling}>
+        <div>{text}</div>
+      </div>
+    </main>
+  );
+}
+
+function DynamicComponent() {
+  const [checked, setChecked] = useState(false);
+
+  const syncedScrolling = useSyncedScrolling();
+
+  return (
+    <>
       <input
         onChange={() => setChecked((v) => !v)}
         checked={checked}
         type="checkbox"
       />
-      <div className="container">
-        <div className="scrollable" {...syncedScrolling}>
-          <div className="text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Pellentesque sed nunc viverra, bibendum orci in, convallis purus.
-            Duis vitae dapibus urna, sit amet finibus nulla. Nam mollis interdum
-            mi. Integer pulvinar velit vitae quam aliquet, sed vulputate arcu
-            feugiat. Suspendisse potenti. Ut posuere egestas eleifend. Fusce
-            porta mollis eros, sagittis porta ex egestas ac. Sed dictum rhoncus
-            nunc sit amet bibendum. Praesent mauris velit, lacinia a rutrum sed,
-            fermentum eget eros. Donec id tempor magna, a tristique orci. Nullam
-            arcu quam, dapibus ut elit et, aliquet aliquet ipsum. Curabitur
-            risus felis, finibus in dignissim non, tempus in quam.
-          </div>
+      <main>
+        <div {...syncedScrolling}>
+          <div>{text}</div>
         </div>
-        <div className="scrollable" {...syncedScrolling}>
-          <div className="text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Pellentesque sed nunc viverra, bibendum orci in, convallis purus.
-            Duis vitae dapibus urna, sit amet finibus nulla. Nam mollis interdum
-            mi. Integer pulvinar velit vitae quam aliquet, sed vulputate arcu
-            feugiat. Suspendisse potenti. Ut posuere egestas eleifend. Fusce
-            porta mollis eros, sagittis porta ex egestas ac. Sed dictum rhoncus
-            nunc sit amet bibendum. Praesent mauris velit, lacinia a rutrum sed,
-            fermentum eget eros. Donec id tempor magna, a tristique orci. Nullam
-            arcu quam, dapibus ut elit et, aliquet aliquet ipsum. Curabitur
-            risus felis, finibus in dignissim non, tempus in quam.
-          </div>
+        <div {...syncedScrolling}>
+          <div>{text}</div>
         </div>
         {checked && (
-          <div className="scrollable" {...syncedScrolling}>
-            <div className="text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Pellentesque sed nunc viverra, bibendum orci in, convallis purus.
-              Duis vitae dapibus urna, sit amet finibus nulla. Nam mollis
-              interdum mi. Integer pulvinar velit vitae quam aliquet, sed
-              vulputate arcu feugiat. Suspendisse potenti. Ut posuere egestas
-              eleifend. Fusce porta mollis eros, sagittis porta ex egestas ac.
-              Sed dictum rhoncus nunc sit amet bibendum. Praesent mauris velit,
-              lacinia a rutrum sed, fermentum eget eros. Donec id tempor magna,
-              a tristique orci. Nullam arcu quam, dapibus ut elit et, aliquet
-              aliquet ipsum. Curabitur risus felis, finibus in dignissim non,
-              tempus in quam.
-            </div>
+          <div {...syncedScrolling}>
+            <div>{text}</div>
           </div>
         )}
-      </div>
-    </div>
+      </main>
+    </>
   );
 }
